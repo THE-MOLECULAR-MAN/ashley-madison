73c71c9559106b9147fcba7173af74846b65c3b2
thonker-r7 <tim@private.local>
2023-10-11 12:21:53 -0400

    removed comments that were causing issues

--- binds/sql-queries/mysql-disable-password.sql
+++ binds/sql-queries/mysql-disable-password.sql
@@ -1,9 +1,2 @@
--- do not include the db
--- mysql -p
-
--- MySQL 5.x
 FLUSH PRIVILEGES;
---SELECT CURRENT_USER();
---SELECT USER();
---SET PASSWORD FOR root@localhost=PASSWORD('');
 SET PASSWORD FOR root=PASSWORD('');
