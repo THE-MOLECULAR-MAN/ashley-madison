01af6c47924026df66c6c4f9b821ab79fce7d344
thonker-r7 <tim@private.local>
2023-10-11 22:42:17 -0600

    added comments about sizes and load times

--- binds/restore-backups.sh
+++ binds/restore-backups.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # Tim H 2023
 
-# backups seem to take about 30 minutes per gigabyte
+# total time is 677 minutes, or about 11.2 hours
+
 # https://stackoverflow.com/questions/1493722/mysql-command-for-showing-current-configuration-variables
 
-# took ? minutes, 1.7 GB
+# TODO: switch this to be a for loop on *.dump files
+# took 74 minutes, 1.7 GB .dump file
 time mysql aminno < /mnt/ashley_dumps_local/aminno_member_email.dump
 
-# took 60 minutes, 2.3 GB
+# took 62 minutes, 2.3 GB .dump file
 time mysql aminno < /mnt/ashley_dumps_local/member_details.dump
 
-# took 304 minutes, 13 GB .dump file
+# took 322 minutes, 13 GB .dump file
 time mysql aminno < /mnt/ashley_dumps_local/aminno_member.dump
 
-# took 37 minutes, 9.5 GB .dump file
+# took 41 minutes, 9.5 GB .dump file
 time mysql aminno < /mnt/ashley_dumps_local/am_am.dump
 
-# took ? minutes, 4.2 GB .dump file
+# took 178 minutes, 4.2 GB .dump file
 time mysql aminno < /mnt/ashley_dumps_local/member_login.dump
+
+echo "restore-backups.sh finished successfully."
