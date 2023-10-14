e462c31759b8c31da8c3346e193a8ae0d77dffdf
thonker-r7 <tim@private.local>
2023-10-13 23:34:38 -0600

    only important columns, sorted by name to find dupes

--- binds/sql-queries/mysql-list-db-schema.sql
+++ binds/sql-queries/mysql-list-db-schema.sql
@@ -1,7 +1,6 @@
+-- orders it in a way to find duplicate columns easily
 select 
-    tab.table_schema as database_schema,
     tab.table_name as table_name,
-    col.ordinal_position as column_id,
     col.column_name as column_name,
     col.data_type as data_type
 from information_schema.tables as tab
@@ -12,5 +11,4 @@ where tab.table_type = 'BASE TABLE'
     and tab.table_schema not in ('information_schema','mysql',
         'performance_schema','sys')
     and tab.table_schema = database() 
-order by tab.table_name,
-    col.ordinal_position;
+order by column_name;
