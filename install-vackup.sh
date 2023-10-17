ad7eb957e4e2edefa186fab1bf468cd5e18f33d8
thonker-r7 <tim@private.local>
2023-10-16 23:06:33 -0400

    initial version

---
+++ install-vackup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Tim H 2023
+
+# https://github.com/BretFisher/docker-vackup
+
+wget https://raw.githubusercontent.com/BretFisher/docker-vackup/main/vackup
+sudo mv ./vackup /usr/local/bin/
+sudo chmod +x /usr/local/bin/vackup
+
+# test it
+vackup
+
+
+
+
+screen -S vackup
+
+# show current size of the db volume
+docker volume ls
+
+docker system df -v
+# original size was 90.97 GB, after 1 full maintanence cycle to shrink it
+# tarbal using GZIP=-2 and otherwise defaults was: 33 GB, took 195 minutes
+# tarbal using GZIP=-9 and otherwise defaults was: 33 GB, took 196 minutes
+# time to SHA256 hash the file: 7 minutes
+
+rm  "ashley-db-backup-test1.tar.gz"
+
+# do not put a path on the output file.
+date
+BACKUP_FILENAME="ashley-db-backup-test2.tar.gz"
+time GZIP=-9 vackup export ashley-madison-db-volume "$BACKUP_FILENAME"
+time sha256sum "$BACKUP_FILENAME" > "${BACKUP_FILENAME}.sha256"
+ls -lah "$BACKUP_FILENAME" "${BACKUP_FILENAME}.sha256"
+date
