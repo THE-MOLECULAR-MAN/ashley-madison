9d321fb1afcfb73f723dd44f5c3a07d61a430782
thonker-r7 <tim@private.local>
2023-10-16 21:36:35 -0400

    revised to \G format

--- binds/sql-queries/single-profile-dump.sql
+++ binds/sql-queries/single-profile-dump.sql
@@ -1,39 +1,38 @@
-SELECT md.dob,
-    md.profile_caption, md.profile_weight, md.profile_height,
-    am.nickname, am.zip, am.city, am.bc_mail_last_time, am.bc_chat_last_time,
-    am.reply_mail_last_time, am.keywords, am.security_answer, 
-    am.opento, am.opento_other, am.opento_abstract, 
-    am.turnsmeon,
-    am.turnsmeon_other,
-    am.turnsmeon_abstract,
-    am.lookingfor,
-    am.lookingfor_other,
-    am.lookingfor_abstract,
-    am.updatedon,
-    aam.createdon,
-    aam.updatedon,
-    aam.phone,
-    aam.pref_opento,
-    aam.pref_opento_other,
-    aam.pref_opento_abstract,
-    aam.pref_turnsmeon,
+-- selected columns for a specific profile
+SELECT 
+    am.pnum,
+    aam.profile_number,
+    ame.email,
+    aam.first_name,
+    aam.last_name,
+    am.nickname,
+    am.gender,
+    am.dob,
+    am.keywords,
+    am.caption,
     aam.pref_turnsmeon_other,
     aam.pref_turnsmeon_abstract,
-    aam.pref_lookingfor,
     aam.pref_lookingfor_other,
     aam.pref_lookingfor_abstract,
-    aam.security_answer,
-    ml.username,
-    ml.password
+    aam.pref_opento_other,
+    aam.pref_opento_abstract,
+    am.ethnicity,
+    am.weight,
+    am.height,
+    am.photos_public,
+    am.photos_private,
+    aam.phone,
+    aam.work_phone,
+    aam.mobile_phone,
+    am.zip,
+    am.latitude,
+    am.longitude,
+    am.city
 FROM
-    aminno_member_email as ame
-JOIN member_details as md
-    on md.pnum = ame.pnum
-JOIN aminno_member as am
-    on am.pnum = ame.pnum
+    aminno_member as am
 JOIN am_am_member as aam
-    on aam.id = ame.pnum
-JOIN member_login as ml
-    on ml.pnum = ame.pnum
-WHERE 
-    EMAIL = 'replaceme@contoso.com';
+    on aam.id = am.pnum
+JOIN aminno_member_email as ame
+    on ame.pnum = am.pnum
+
+WHERE ame.pnum = 123456 \G
