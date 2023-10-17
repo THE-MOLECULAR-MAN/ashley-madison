306d0ebf67e2cd48818d4541026bf98275596869
thonker-r7 <tim@private.local>
2023-10-16 22:42:14 -0400

    added output from query in comments

--- binds/sql-queries/count-frauds.sql
+++ binds/sql-queries/count-frauds.sql
@@ -1,3 +1,10 @@
+-- display the counts of each type of fraud
+-- 
+
+
+-- select count(*) FROM
+--     aminno_member;
+
 SELECT 
     fraud_flag,
     count(*) as row_count
@@ -5,4 +12,32 @@ FROM
     aminno_member as am
 GROUP by fraud_flag
 ORDER by row_count desc;
+
+
+-- +------------+-----------+
+-- | fraud_flag | row_count |
+-- +------------+-----------+
+-- |          0 |  36380665 |
+-- |         64 |      5697 |
+-- |         68 |      4776 |
+-- |          1 |      4427 |
+-- |         70 |      1925 |
+-- |         66 |       596 |
+-- |         69 |       483 |
+-- |         65 |       331 |
+-- |         67 |        41 |
+-- |          2 |        38 |
+-- |          5 |        37 |
+-- |         71 |        37 |
+-- |          4 |        23 |
+-- |        324 |        22 |
+-- |          7 |        14 |
+-- |        320 |         3 |
+-- |          6 |         2 |
+-- |        326 |         1 |
+-- +------------+-----------+
+-- 18 rows in set (0.00 sec)
+-- total: 36399118
+-- 18,453 rows have a non-zero/non-null fraud flag
+-- Therefore about 0.05% of the profiles have been flagged as fraud
 
